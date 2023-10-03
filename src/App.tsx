128ff38f1ed5e3bc227ed68a03121e882bcebf10
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-03 01:13:45 -0400

    map anim to skills, contact and projects section

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import { Canvas } from '@react-three/fiber';
 function App() {
   const blobRef = useRef<HTMLDivElement>(null);
     const skillsSection = document.querySelector('.skills-section');
+    const contactSection = document.querySelector('#contact');
+    const miscDiv = document.querySelector('.misc-div')
 
   useLayoutEffect(() => {
  const ctx = gsap.context(() => {
@@ -18,14 +20,49 @@ function App() {
   // gsap.set(blobRef.current, {})
    gsap.to(blobRef.current, {
      ease: 'none',
-     x: 300,
+     x: 500,
+    //  scaleY: 3,
+    //  scaleX: 3,
+   
      scrollTrigger: {
        trigger: skillsSection,
-       start: 'bottom bottom ',
+       start: 'top center',
        end: 'center bottom',
-       markers: true,
+
+
+      //  markers: true,
+       scrub:4, 
+       pin:true
      },
    });
+
+
+      // gsap.to(blobRef.current, {
+      //   ease: 'none',
+      //   x: -5,
+      //   scrollTrigger: {
+      //     trigger: contactSection,
+      //     start: 'top 10%',
+      //     end: 'top center',
+      //     // markers: true,
+      //     scrub: true,
+      //     pin: true,
+      //   },
+      // });
+            gsap.to(blobRef.current, {
+          xPercent: -200,
+          ease: 'none',
+          duration: 2,
+          scrollTrigger: {
+            trigger: '.misc-div',
+            start: 'top top',
+            end: '50% 10%',
+            scrub: false,
+            pin:true
+            // scrub: true,
+            // markers: true,
+          },
+        });
  });
 
  return () => {
