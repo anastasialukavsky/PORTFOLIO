822f9abdfd6efa35c70294274c1ee0ab2a0de5be
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-03 22:18:55 -0400

    clean up component, move blob logic to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,33 @@
 import React, { useLayoutEffect, useEffect, useRef } from 'react';
 
-import './App.css';
+// import './index.css';
 import Homepage from './Homepage';
 import Test from './Test';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Blob from './Blob';
 import { Canvas } from '@react-three/fiber';
+import ProgressBar from './ProgressBar';
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
 
 function App() {
-  const blobRef = useRef<HTMLDivElement>(null);
-    const skillsSection = document.querySelector('.skills-section');
-    const contactSection = document.querySelector('#contact');
-    const miscDiv = document.querySelector('.misc-div')
-    const projectsSection = document.querySelector('.projects-section')
 
-  useLayoutEffect(() => {
- const ctx = gsap.context(() => {
-
-  // gsap.set(blobRef.current, {})
-   gsap.to(blobRef.current, {
-     ease: 'none',
-     x: 500,
-    //  scaleY: 3,
-    //  scaleX: 3,
-   
-     scrollTrigger: {
-       trigger: skillsSection,
-       start: 'top center',
-       end: 'center bottom',
-
-
-      //  markers: true,
-       scrub:4, 
-       pin:true
-     },
-   });
-
-
-      // gsap.to(blobRef.current, {
-      //   ease: 'none',
-      //   x: -5,
-      //   scrollTrigger: {
-      //     trigger: contactSection,
-      //     start: 'top 10%',
-      //     end: 'top center',
-      //     // markers: true,
-      //     scrub: true,
-      //     pin: true,
-      //   },
-      // });
-
-
-
-        //     gsap.to(blobRef.current, {
-        //   xPercent: -100,
-        //   ease: 'none',
-        //   duration: 2,
-        //   scrollTrigger: {
-        //     trigger: contactSection,
-        //     start: 'top top',
-        //     end: 'center center',
-        //     // scrub: false,
-        //     pin:true,
-        //     scrub: 5,
-        //     markers: true,
-        //   },
-        // });
-
-        
- });
-
- return () => {
-   ScrollTrigger.refresh();
-   ctx.revert();
- };
-  }, []);
 
   return (
     <div className='min-h-screen h-full bg-[#121212] w-screen relative'>
-      <div ref={blobRef} className='blob-wrapper fixed   h-full  z-[100] w-full  top-[1/2] right-[1/2]'>
-        <Canvas camera={{ position: [0.0, 0.0, 8.0] }} >
-          <Blob />
-        </Canvas>
-      </div>
+
+
+
+      {/* <ProgressBar /> */}
+
       <Homepage />
     </div>
   );
 }
+
 
 export default App;
