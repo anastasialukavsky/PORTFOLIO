83cb766554de6294688f7ea2e998f40f37698516
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-03 22:19:35 -0400

    add new component

---
+++ src/ProgressBar.tsx
@@ -0,0 +1,10 @@
+// import whiteDot from '../public/icons/white_dot.svg';
+// import blackDot from '../public/icons/black_dot.svg';
+
+export default function ProgressBar() {
+  return (
+    <div className='progress-bar fixed top-[20%] left-0   h-full z-[300] w-4 bg-pink-300 '>
+      <div className='' ></div>
+    </div>
+  );
+}
