ff630295aca3c14fec9355f90b5f312f76c644a5
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-06 02:05:33 -0400

    add lenis config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useLayoutEffect, useEffect, useRef } from 'react';
 
 // import './index.css';
 import Homepage from './Homepage';
+import Lenis from '@studio-freight/lenis';
 import Test from './Test';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -16,7 +17,34 @@ gsap.registerPlugin(ScrollTrigger);
 
 function App() {
 
+   const lenis = new Lenis({
+     duration: 2.2,
+     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+     // orientation: 'vertical',
+     // gestureOrientation: 'vertical',
+     wheelMultiplier: 1,
+     smoothTouch: false,
+     touchMultiplier: 2,
+     infinite: false,
+     // lerp: 3,
+     // duration: 1,
+     smoothWheel: true,
+   });
 
+   //   lenis.on('scroll', ScrollTrigger.update);
+
+   // lenis.on('scroll', (e: any) => {
+    // bg - [#a977cb];
+    // [#1d1d1d]
+   //   // console.log(e);
+   // });
+
+   function raf(time: number) {
+     lenis.raf(time);
+     requestAnimationFrame(raf);
+   }
+
+   requestAnimationFrame(raf);
   return (
     <div className='min-h-screen h-full bg-[#121212] w-screen relative'>
 
