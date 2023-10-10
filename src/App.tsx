9a070dc40baf9075048a490e7c402eff8dac8549
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-09 20:41:46 -0400

    add cursor customization

--- src/App.tsx
+++ src/App.tsx
@@ -16,42 +16,82 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 function App() {
+  const lenis = new Lenis({
+    duration: 2.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    // orientation: 'vertical',
+    // gestureOrientation: 'vertical',
+    wheelMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+    // lerp: 3,
+    // duration: 1,
+    smoothWheel: true,
+  });
 
-   const lenis = new Lenis({
-     duration: 2.2,
-     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-     // orientation: 'vertical',
-     // gestureOrientation: 'vertical',
-     wheelMultiplier: 1,
-     smoothTouch: false,
-     touchMultiplier: 2,
-     infinite: false,
-     // lerp: 3,
-     // duration: 1,
-     smoothWheel: true,
-   });
+  //   lenis.on('scroll', ScrollTrigger.update);
 
-   //   lenis.on('scroll', ScrollTrigger.update);
+  // lenis.on('scroll', (e: any) => {
+  // bg - [#a977cb];
+  // 343a40
+  // 353b3c
+  // [#1d1d1d]
+  //   // console.log(e);
+  // });
 
-   // lenis.on('scroll', (e: any) => {
-    // bg - [#a977cb];
-    // 343a40
-    // 353b3c
-    // [#1d1d1d]
-   //   // console.log(e);
-   // });
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
 
-   function raf(time: number) {
-     lenis.raf(time);
-     requestAnimationFrame(raf);
-   }
+  requestAnimationFrame(raf);
 
-   requestAnimationFrame(raf);
+  useEffect(() => {
+    gsap.set('.ball', { xPercent: -50, yPercent: -50 });
+
+    const ball = document.querySelector('.ball');
+    const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    const mouse = { x: pos.x, y: pos.y };
+    const speed = 0.14;
+
+    const xSet = gsap.quickSetter(ball, 'x', 'px');
+    const ySet = gsap.quickSetter(ball, 'y', 'px');
+
+    const handleMouseMove = (e: MouseEvent) => {
+      mouse.x = e.x;
+      mouse.y = e.y;
+    };
+
+    const updateBallPosition = () => {
+      const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+      pos.x += (mouse.x - pos.x) * dt;
+      pos.y += (mouse.y - pos.y) * dt;
+      xSet(pos.x);
+      ySet(pos.y);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    const tickerUpdate = () => {
+      updateBallPosition();
+    };
+
+    gsap.ticker.add(tickerUpdate);
+  // ball?.style.pointerEvents = 'none';
+   
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      gsap.ticker.remove(tickerUpdate);
+    };
+  }, []); 
+
   return (
     <div className='min-h-screen h-full bg-[#353b3c] w-screen relative portrait:w-[100vw]'>
       {/* <ProgressBar /> */}
 
       <Homepage />
+      <div className='ball mix-blend-difference w-[60px] h-[60px] fixed bg-white top-0 left-0 rounded-full pointer-events-none'></div>
     </div>
   );
 }
