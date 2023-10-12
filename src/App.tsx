6528f18fa245c0e08a8c4dfaa4e662cf4432b8bc
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-12 14:37:30 -0400

    add lenis gsap scrollTrigger intergation

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,14 @@ function App() {
   //   // console.log(e);
   // });
 
+  lenis.on('scroll', ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
   function raf(time: number) {
     lenis.raf(time);
     requestAnimationFrame(raf);
