9a931a4b1a05668624109a8fd120ed952c8f0ed4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-13 17:40:42 -0400

    set upheight diff mesurer, add state to sibling stack compoent

--- src/ProjectDescription.tsx
+++ src/ProjectDescription.tsx
@@ -5,49 +5,59 @@ export type ProjectDescProps = {
   projectDescHeight: number;
   setProjectDescHeight: React.Dispatch<React.SetStateAction<number>>;
   setIsFullDescription: React.Dispatch<React.SetStateAction<boolean>>;
+  isFullDescription: boolean;
 };
 export default function ProjectDescription({
   description,
+  isFullDescription,
+  setIsFullDescription,
   setProjectDescHeight,
   projectDescHeight,
-  setIsFullDescription
 }: ProjectDescProps) {
   const [toggleDesc, setToggleDesc] = useState(true);
-
   const descRef = useRef<HTMLDivElement>(null);
+
+
+  //set initial projectHeight to 0
   const [shortDescriptionHeight, setShortDescriptionHeight] = useState(0);
   const [fullDescriptionHeight, setFullDescriptionHeight] = useState(0);
 
-  let shortDescription = description.slice(0, 200);
+  let shortDescription = description.slice(0, 200) + '...';
   let fullDescription = description;
 
   const handleFullDescription = () => {
-    if (shortDescription) {
-      setToggleDesc((prev) => !prev);
-    }
+    setToggleDesc((prev) => !prev);
+    setIsFullDescription((prev) => !prev);
   };
 
   useEffect(() => {
-    if (descRef.current) {
-      // console.log('h', descRef.current.getBoundingClientRect());
-      setShortDescriptionHeight(descRef.current.getBoundingClientRect().height);
-      // setIsFullDescription(false)
-    }
-  }, [description]);
+    if (!descRef.current) return;
 
-  useEffect(() => {
-    if (descRef.current) {
-      setFullDescriptionHeight(descRef.current.getBoundingClientRect().height)
-      // setIsFullDescription(true)
+    if (toggleDesc) {
+      setProjectDescHeight(0);
+      setShortDescriptionHeight(descRef.current.getBoundingClientRect().height);
+    } else {
+      setFullDescriptionHeight(descRef.current.getBoundingClientRect().height);
+      setProjectDescHeight(heightDifference);
     }
-  }, [toggleDesc, description]);
+  }, [toggleDesc, projectDescHeight]);
+  
+  // console.log(descRef.current)
+  // console.log({ isFullDescription });
+  // console.log({ shortDescriptionHeight });
+  // console.log({ fullDescriptionHeight });
+  // console.log({ shortDescription });
+  // console.log({ fullDescription });
   
   const heightDifference = fullDescriptionHeight - shortDescriptionHeight;
+  // console.log({ heightDifference });
   
-  useEffect(() => {
-
-    setProjectDescHeight(heightDifference);
-  }, [heightDifference])
+  
+  console.log({projectDescHeight})
+  // useEffect(() => {
+  //   // setProjectDescHeight(heightDifference);
+  //   // console.log({ heightDifference });
+  // }, [heightDifference, projectDescHeight]);
   // console.log('diff', heightDifference);
 
   return (
