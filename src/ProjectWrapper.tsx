ce92baf40e9a850536c3a0e011718f64cec10f9d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-13 17:41:35 -0400

    add sibling components,. drilled props

--- src/ProjectWrapper.tsx
+++ src/ProjectWrapper.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import ProjectDescription from './ProjectDescription'
-import ProjectStack from './ProjectStack'
+import React, { useState } from 'react';
+import ProjectDescription from './ProjectDescription';
+import ProjectStack from './ProjectStack';
+import { Logo } from './ProjectStack';
 
-export default function ProjectWrapper() {
+export type ProjectDescProps = {
+  description: string;
+  frontEnd: Logo[];
+  backEnd: Logo[];
+};
+
+export default function ProjectWrapper({
+  description,
+  frontEnd,
+  backEnd,
+}: ProjectDescProps) {
+  const [projectDescHeight, setProjectDescHeight] = useState(0);
+  const [isFullDescription, setIsFullDescription] = useState(false);
+
+  // console.log('diff', projectDescHeight);
+
   return (
-    <div>
-      <ProjectDescription/>
-      <ProjectStack/>
-    </div>
-  )
+    <>
+      <ProjectDescription
+        description={description}
+        setIsFullDescription={setIsFullDescription}
+        isFullDescription={isFullDescription}
+        projectDescHeight={projectDescHeight}
+        setProjectDescHeight={setProjectDescHeight}
+      />
+
+      <ProjectStack
+        frontEnd={frontEnd}
+        backEnd={backEnd}
+        isFullDescription={isFullDescription}
+        projectDescHeight={projectDescHeight}
+      />
+    </>
+  );
 }
