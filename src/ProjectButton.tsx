5cbd1b2ba922b1bf8d52f21c09810a5392b04cdf
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-14 13:54:59 -0400

    add resp styles

--- src/ProjectButton.tsx
+++ src/ProjectButton.tsx
@@ -7,7 +7,7 @@ export type ProjectBtnProps = {
 
 export default function ProjectButton({ link }: ProjectBtnProps) {
   return (
-    <div className='cursor-pointer flex  w-fit self-end h-fit items-center align-baseline pb-2'>
+    <div className='cursor-pointer flex  w-fit self-end h-fit items-center align-baseline pb-2 xl:pb-4 4xl:pb-8 5xl:pr-[8%] 6xl:pb-0 6xl:pr-[9%]'>
       <a
         id='project-link'
         href={link}
@@ -16,12 +16,12 @@ export default function ProjectButton({ link }: ProjectBtnProps) {
         className={` 
                    group relative  inline-block  max-w-full  font-medium text-[#121212] focus:outline-none `}
       >
-        <span className='absolute inset-0 border border-[#353939]] bg-[#353939]  z-0 '></span>
+        <span className='absolute inset-0 border border-[#353939] bg-[#353939]  z-0 '></span>
         <span
           className={` 
-   border border-[#121212]  px-2  relative  -translate-x-1 -translate-y-1  4xl:py-4 5xl:text-[1.3vw] 6xl:text-[1.1vw] flex py-1 z-20 bg-white gap-2 lg:text-[.7rem] text-[.5rem]`}
+   border border-[#121212]  px-2 4xl:px-3 relative  -translate-x-1 -translate-y-1  4xl:py-2 5xl:text-[.9rem]  flex py-1 z-20 bg-white gap-2 lg:text-[.7rem] text-[.5rem] 2xl:text-[.8rem] font-mono`}
         >
-          project link <img src={redirect} alt='' className='w-[10px] lg:h-3' />
+          project link <img src={redirect} alt='' className='w-[10px] xl:w-[11px] 2xl:w-[12px] 5xl:w-[16px]' />
         </span>
       </a>
     </div>
