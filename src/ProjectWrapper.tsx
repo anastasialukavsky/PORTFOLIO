bfb32102733cb6eab2866d9f527ffc628e7c7ef2
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-13 23:11:47 -0400

    drill props to children

--- src/ProjectWrapper.tsx
+++ src/ProjectWrapper.tsx
@@ -16,20 +16,30 @@ export default function ProjectWrapper({
 }: ProjectDescProps) {
   const [projectDescHeight, setProjectDescHeight] = useState(0);
   const [isFullDescription, setIsFullDescription] = useState(false);
+    const [shortDescriptionHeight, setShortDescriptionHeight] = useState(0);
+    const [fullDescriptionHeight, setFullDescriptionHeight] = useState(0);
 
   // console.log('diff', projectDescHeight);
 
   return (
     <>
       <ProjectDescription
+        shortDescriptionHeight={shortDescriptionHeight}
+        fullDescriptionHeight={fullDescriptionHeight}
+        setShortDescriptionHeight={setShortDescriptionHeight}
+        setFullDescriptionHeight={setFullDescriptionHeight}
         description={description}
         setIsFullDescription={setIsFullDescription}
         isFullDescription={isFullDescription}
         projectDescHeight={projectDescHeight}
         setProjectDescHeight={setProjectDescHeight}
       />
-
+      <p className='text-center text-[.8rem] lg:text-[1rem] 2xl:text-[1.3rem] pt-3'>
+        STACK
+      </p>
       <ProjectStack
+        shortDescriptionHeight={shortDescriptionHeight}
+        fullDescriptionHeight={fullDescriptionHeight}
         frontEnd={frontEnd}
         backEnd={backEnd}
         isFullDescription={isFullDescription}
