a06736dbc9d356656c0c6133013ca84f662e1ca6
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-13 23:10:47 -0400

    add new project header component

---
+++ src/ProjectHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export type ProjectHeaderProps = {
+  projectName: string
+}
+export default function ProjectHeader({projectName}: ProjectHeaderProps) {
+  return (
+    <div>
+      <p className='text-center md:text-[1rem] lg:text-[1.2rem] 2xl:text-[2rem] text-[5vw] pt-5 md:pt-0 short:text-[1.6rem] text-[#121212] font-mono font-black'>
+        {projectName}
+      </p>
+    </div>
+  );
+}
