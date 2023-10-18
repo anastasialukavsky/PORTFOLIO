edaf0f014fa3199cfba4e8b9badb28dea196e18e
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-17 23:26:12 -0400

    exctract anim into custom hook

---
+++ src/useNavLinksAnimation.ts
@@ -0,0 +1,54 @@
+
+import { useEffect } from "react";
+import SplitType from "split-type";
+import { gsap } from "gsap";
+
+export function useNavLinksAnimation() {
+
+ useEffect(() => {
+   const navLinks = document.querySelectorAll('.nav-text');
+
+   navLinks.forEach((navLink) => {
+     const splitText = new SplitType(navLink as HTMLElement, { types: 'chars' });
+     const chars = splitText.chars;
+
+     const handleHover = () => {
+       gsap.set(chars, {
+         transformOrigin: 'center center -10px',
+         backfaceVisibility: 'hidden',
+       });
+
+       gsap.to(chars, {
+         rotationX: '360',
+         duration: 0.5,
+         stagger: 0.04,
+         ease: 'slow.inOut',
+       });
+     };
+
+     navLink.addEventListener('mouseenter', handleHover);
+
+     navLink.addEventListener('mouseleave', () => {
+       gsap.to(chars, {
+         rotationX: '0',
+         duration: 0,
+         stagger: 0.04,
+         ease: 'slow',
+       });
+     });
+
+     return () => {
+       navLink.removeEventListener('mouseenter', handleHover);
+       navLink.removeEventListener('mouseleave', () => {
+         gsap.to(chars, {
+           rotationX: '0',
+           duration: 0,
+           stagger: 0.04,
+           ease: 'slow',
+         });
+       });
+     };
+   });
+ }, []);
+
+}
