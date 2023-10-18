70377a885a40fd87f64c61ede8f653380f9ede7f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-17 21:29:05 -0400

    add separatr logic for logos

---
+++ src/logos.ts
@@ -0,0 +1,84 @@
+import html from '../public/icons/html-1.svg';
+import axios from '../public/icons/axios.svg';
+import blendr from '../public/icons/blender-1.svg';
+import css from '../public/icons/css-3.svg';
+import docker from '../public/icons/docker.svg';
+import electron from '../public/icons/electron-1.svg';
+import figma from '../public/icons/figma-5.svg';
+import git from '../public/icons/git-icon.svg';
+import gsapIcon from '../public/icons/gsap.svg';
+import javascript from '../public/icons/javascript-1.svg';
+import mongodb from '../public/icons/mongodb.svg';
+import nginx from '../public/icons/nginx.svg';
+import nodejs from '../public/icons/nodejs-1.svg';
+import postreSQL from '../public/icons/postgresql.svg';
+import prisma from '../public/icons/prisma-3.svg';
+import reactIcon from '../public/icons/react-2.svg';
+import reactNative from '../public/icons/react-native-1.svg';
+import redux from '../public/icons/redux.svg';
+import sequelize from '../public/icons/sequelize.svg';
+import stripe from '../public/icons/stripe-3.svg';
+import tailwind from '../public/icons/tailwind-css-2.svg';
+import threejs from '../public/icons/threejs-1.svg';
+import typescript from '../public/icons/ts-logo-512.svg';
+import vite from '../public/icons/vitejs.svg';
+import zod from '../public/icons/zod.svg';
+import postman from '../public/icons/postman.svg';
+import mongoose from '../public/icons/mongoose.svg';
+import express from '../public/icons/express-logo.svg';
+import rht from '../public/icons/rht.svg';
+import lenis from '../public/icons/lenis.svg';
+import fusejs from '../public/icons/fuse.svg';
+import framer from '../public/icons/framer-motion.svg';
+import passportjs from '../public/icons/passport.svg';
+import jwt from '../public/icons/jwt-3.svg';
+import peerjs from '../public/icons/peerjs.svg';
+import tweetnacl from '../public/icons/tweet.svg';
+import reactFiber from '../public/icons/fiber.svg';
+import socketio from '../public/icons/socket-io.svg';
+import yelpFusionAPI from '../public/icons/yelp-1.svg';
+import googleMapsAPI from '../public/icons/google-maps.svg';
+
+export const logos = {
+  NodeJS: nodejs,
+  HTML: html,
+  Axios: axios,
+  Blender: blendr,
+  CSS: css,
+  Docker: docker,
+  Electron: electron,
+  Figma: figma,
+  Git: git,
+  GSAP: gsapIcon,
+  JavaScript: javascript,
+  MongoDB: mongodb,
+  Nginx: nginx,
+  PostgreSQL: postreSQL,
+  Prisma: prisma,
+  React: reactIcon,
+  ReactNative: reactNative,
+  Redux: redux,
+  Sequelize: sequelize,
+  Stripe: stripe,
+  TailwindCSS: tailwind,
+  ThreeJS: threejs,
+  TypeScript: typescript,
+  Vite: vite,
+  Zod: zod,
+  Postman: postman,
+  Mongoose: mongoose,
+  ExpressJS: express,
+  ReactHotToast: rht,
+  Lenis: lenis,
+  FuseJS: fusejs,
+  FramerMotion: framer,
+  PassportJS: passportjs,
+  JWT: jwt,
+  TweetNaCl: tweetnacl,
+  PeerJS: peerjs,
+  ReactFiber: reactFiber,
+  SocketIO: socketio,
+  Blendr: blendr,
+  YelpFusionAPI: yelpFusionAPI,
+  GoogleMapsAPI: googleMapsAPI,
+};
