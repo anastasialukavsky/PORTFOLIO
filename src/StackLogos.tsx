ed8cf8e6c87a84d8ce56c33923a6ac3e85491f02
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-18 21:45:26 -0400

    add resp styles

--- src/StackLogos.tsx
+++ src/StackLogos.tsx
@@ -15,7 +15,7 @@ export default function StackLogos({item, logoIcon}: StackLogosProps) {
         alt=''
         className='h-6 w-6 lg:h-8 lg:w-8 xl:h-10 xl:w-11 2xl:w-16 short:w-10 short:h-8 xshort:h-4 xshort:w-8 portrait:w-16 portrait:h-8'
         />
-      <p className='text-center  text-[.5rem] lg:text-[.7rem] xl:text-[.8rem] 5xl:text-[.9rem] 6xl:text-[1rem] portrait:text-[.9rem]'>
+      <p className='text-center  text-[.5rem] lg:text-[.7rem] xl:text-[.9rem] 5xl:text-[.9rem] 6xl:text-[1rem] portrait:text-[.9rem]'>
         {item}
       </p>
         </>
