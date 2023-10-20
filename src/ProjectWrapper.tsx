73e9b1bd571e96e40512cc36d2443ee970ff2a68
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-20 14:22:07 -0400

    rm unused code

--- src/ProjectWrapper.tsx
+++ src/ProjectWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, } from 'react';
 import ProjectDescription from './ProjectDescription';
 import ProjectStack from './ProjectStack';
 import { Logo } from './ProjectStack';
@@ -14,42 +14,12 @@ export default function ProjectWrapper({
   frontEnd,
   backEnd,
 }: ProjectDescProps) {
-  const [projectDescHeight, setProjectDescHeight] = useState(0);
   const [isFullDescription, setIsFullDescription] = useState(false);
   const [shortDescriptionHeight, setShortDescriptionHeight] = useState(0);
-  const [fullDescriptionHeight, setFullDescriptionHeight] = useState(0);
-  const [fullStackHeight, setFullStackHeight] = useState(0);
-  const [shortStackHeight, setShortStackHeight] = useState(0);
   const [toggleDesc, setToggleDesc] = useState(false);
   const descRef = useRef<HTMLDivElement>(null);
   const stackRef = useRef<HTMLDivElement>(null);
 
-  // const projectWrapperRef = useRef<HTMLDivElement | null>(null);
-  // const [wrapperHeight, setWrapperHeight] = useState(0);
-
-  // useEffect(() => {
-  //   const height = projectWrapperRef.current?.getBoundingClientRect().height;
-  //   if (height) setWrapperHeight(height);
-  // }, []);
-
-  // useEffect(() => {
-  //   // const height = descRef.current?.getBoundingClientRect().height || 0;
-  //   // setShortStackHeight(stackRef.current?.getBoundingClientRect().height || 0)
-  //   setTimeout(() => {
-  //     setFullDescriptionHeight(
-  //       descRef.current?.getBoundingClientRect().height || 0
-  //     );
-  //     setToggleDesc(false);
-  //     setFullStackHeight(stackRef.current?.getBoundingClientRect().height || 0);
-  //   }, 1000);
-  // }, []);
-
-
-  // console.log({fullDescriptionHeight})
-  // console.log({shortDescriptionHeight})
-
-  // console.log({shortStackHeight})
-  // console.log({fullStackHeight})
   return (
     <>
       <ProjectDescription
@@ -57,32 +27,18 @@ export default function ProjectWrapper({
         toggleDesc={toggleDesc}
         setToggleDesc={setToggleDesc}
         shortDescriptionHeight={shortDescriptionHeight}
-        fullDescriptionHeight={fullDescriptionHeight}
         setShortDescriptionHeight={setShortDescriptionHeight}
-        setFullDescriptionHeight={setFullDescriptionHeight}
         description={description}
         setIsFullDescription={setIsFullDescription}
         isFullDescription={isFullDescription}
-        projectDescHeight={projectDescHeight}
-        setProjectDescHeight={setProjectDescHeight}
       />
       <p className='text-center text-[.8rem] lg:text-[1rem] 2xl:text-[1.3rem] pt-3 font-bold xshort:pt-0 portrait:text-[1.1rem]'>
         STACK
       </p>
       <ProjectStack
         ref={stackRef}
-        // wrapperHeight={wrapperHeight}
-        toggleDesc={toggleDesc}
-        setShortStackHeight={setShortStackHeight}
-        shortStackHeight={shortStackHeight}
-        fullStackHeight={fullStackHeight}
-        setFullStackHeight={setFullStackHeight}
-        shortDescriptionHeight={shortDescriptionHeight}
-        fullDescriptionHeight={fullDescriptionHeight}
         frontEnd={frontEnd}
         backEnd={backEnd}
-        isFullDescription={isFullDescription}
-        projectDescHeight={projectDescHeight}
       />
     </>
   );
