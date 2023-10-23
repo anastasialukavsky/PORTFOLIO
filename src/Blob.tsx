cd30dee921fea61f2505ad08634f6f8b601224c7
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-23 18:43:31 -0400

    cleanup

--- src/Blob.tsx
+++ src/Blob.tsx
@@ -15,10 +15,9 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 import { useThree } from '@react-three/fiber';
-// import { AttrPlugin } from 'gsap/AttrPlugin';
 
 gsap.registerPlugin(ScrollTrigger);
-// import { useControls } from 'leva';
+
 
 export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
   const mesh = useRef<Mesh | null>(null);
@@ -28,7 +27,7 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
     wireframe: true,
   };
 
-  // const [opacity, setOpacity] = useState(1);
+ 
 
   const uniforms = useMemo(() => {
     return {
@@ -41,8 +40,7 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
     const { clock } = state;
     if (mesh.current) {
       const material = mesh.current.material as ShaderMaterial;
-      // material.opacity = materialProps.opacity;
-      // material.transparent = materialProps.opacity < 1;
+
       material.uniforms.u_time.value = 0.4 * clock.getElapsedTime();
       material.uniforms.u_intensity.value = MathUtils.lerp(
         material.uniforms.u_intensity.value,
@@ -103,16 +101,7 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
         y: -(mousePosition.y / window.innerHeight) * 2 + 1,
       };
 
-      // mesh.current.position.x = lerp(
-      //   currentPosition.x,
-      //   targetPosition.x,
-      //   lerpFactor
-      // );
-      // mesh.current.position.y = lerp(
-      //   currentPosition.y,
-      //   targetPosition.y,
-      //   lerpFactor
-      // );
+
     }
   });
 
@@ -122,38 +111,22 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
       mesh.current.position.x = (mousePosition.x / window.innerWidth) * 3 - 1;
       mesh.current.position.y = -(mousePosition.y / window.innerHeight) * 2 + 1;
 
-      // console.log(mesh.current.scale)
-      // mesh.current.scale.set(2.2 * scale, 2.2 * scale, 2.2 * scale)
     }
   });
-  // console.log('mesh', mesh.current);
+
 
   useEffect(() => {
-    if (!mesh.current) return; // Ensure mesh.current is available
+    if (!mesh.current) return; 
     const ctx = gsap.context(() => {
       gsap.to(mesh.current?.rotation, {
-        // x: '+=20',
+ 
         y: '+=.2',
         repeat: -1,
         ease: 'none',
         repeatRefresh: true,
-        //  scrollTrigger: {
-        //   trigger: aboutSection,
-        //   start: 'center center',
-        //   end: 'bottom 140%',
-        //   scrub: 3,
-        // },
+
       });
 
-      // gsap.to(mesh.current, {
-      //   // rotate: 900,
-      //   scrollTrigger: {
-      //     trigger: aboutSection,
-      //     start: 'center center',
-      //     end: 'bottom 140%',
-      //     scrub: 3,
-      //   },
-      // });
     }, mesh);
 
     return () => ctx.revert();
@@ -166,51 +139,19 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
 
 
   const { camera } = useThree();
-  let meshPosition = mesh.current ? mesh.current.position : new THREE.Vector3();
 
-  // console.log('meshpos', camera);
+
 
   const position = { x: 0, y: 0, z: 9.9 };
   useLayoutEffect(() => {
     gsap.registerPlugin({
       scrollTrigger: ScrollTrigger,
-      // xPercent: gsap.utils.checkPrefix('xPercent'),
-      // yPercent: gsap.utils.checkPrefix('yPercent'),
+
     });
 
     if (!mesh.current || !aboutSection) return;
     const ctx = gsap.context(() => {
-      let prevPos = 0;
 
-      // gsap.to(position, {
-      //   // yPercent: 70,
-      //   // z: .1,
-      //   x: 40,
-      //   // y: 1,
-      //   repeatRefresh: true,
-      //   scrollTrigger: {
-      //     trigger: aboutSection,
-      //     start: 'center center',
-      //     end: 'bottom 140%',
-      //     scrub: 3,
-
-      //   },
-      //   onUpdate: () => {
-      //     // console.log('mesh', mesh.current)
-      //     // mesh.current?.translateY(position.y - mesh.current?.position.y)
-      //     // mesh.current?.translateZ(position.z - mesh.current?.position.z)
-      //     mesh.current?.translateX(position.x - mesh.current?.position.x)
-      //     // prevPos = position.z
-      //     // prevPos = position.x
-      //     // console.log(prevPos)
-      //     console.log(mesh.current?.position)
-      //     // mesh.current?.translateZ(position.z);
-      //     // mesh.current?.translateY(position.x);
-      //     // mesh.current!.position = new THREE.Vector3(0,0,position.z);
-      //     // mesh.current?.updateMatrix();
-      //   }
-      // });
-      // const newPosition = { x: -4.9, y: 1, z: 6 };
       gsap.from(position, {
         z: 0,
         x: 0,
@@ -242,14 +183,13 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
           camera.translateX(position.x - camera.position.x);
         },
       });
-      // let zoom = 1;
-      // // const nzoom = { x: 1, y: 1, z: 3 };
+
       const prevPosition = { x: -5.3, y: 1, z: 3 };
       const mobilePrevPosition = {x:-4 , y:8,  z: 6}
       gsap.to(mobileMenu ? mobilePrevPosition : prevPosition, {
         repeatRefresh: true,
         z: mobileMenu ? 6 : 15,
-        x: mobileMenu ? 3 : -2.9,
+        x: mobileMenu ? 3 : -4.9,
         y: mobileMenu ? 5 : 0,
         scrollTrigger: {
           trigger: miscSection,
@@ -257,15 +197,12 @@ export default function Blob({ mobileMenu }: { mobileMenu: boolean }) {
           end: mobileMenu ? 'bottom bottom' : '',
           endTrigger: contactSection,
           scrub: 3,
-          // markers: true,
+      
         },
         onUpdate: () => {
           mobileMenu ? camera.translateY(mobilePrevPosition.y - camera.position.y) : camera.translateY(prevPosition.y - camera.position.y);
           mobileMenu ? camera.translateZ(mobilePrevPosition.z - camera.position.z) : camera.translateZ(prevPosition.z - camera.position.z);
           mobileMenu ? camera.translateX(mobilePrevPosition.x - camera.position.x) : camera.translateX(prevPosition.x - camera.position.x);
-          //  console.log(camera.prevPosition);
-          //  console.log('NP', prevPosition);
-          //  console.log()
           
         },
       });
