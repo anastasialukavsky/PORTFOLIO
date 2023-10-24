d5c9411c945e9dab7934608b869b6e9e97667bff
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-23 20:10:34 -0400

    fix resp styles

--- src/ContactSection.tsx
+++ src/ContactSection.tsx
@@ -20,7 +20,7 @@ export default function ContactSection() {
       <div className='h-screen w-screen  relative '>
         <div className='flex  justify-between  portrait:bg-none bg-cover h-[100svh] portrait:mx-5   mx-36 items-center '>
           <div className=' h-screen w-full landscape:basis-1/2  relative z-[120] gap-9 text-white mix-blend-difference flex flex-col items-center justify-center overflow-x-hidden landscape:translate-x-[25%]'>
-            <p className='md:text-[1.5vw] text-[5vw] 3xl:text-[1vw] whitespace-nowrap'>
+            <p className='md:text-[1.5vw] text-[5vw] 3xl:text-[1vw] whitespace-nowrap portrait:md:text-[1.7rem]'>
               let's connect
             </p>
             <a
@@ -36,7 +36,7 @@ export default function ContactSection() {
               <img
                 src={linkedin}
                 alt=''
-                className=' md:w-[4vw] 5xl:w-[3vw] w-[10vw]'
+                className=' landscape:md:w-[4vw] 5xl:w-[3vw] w-[10vw]'
               />
             </a>
             <a
@@ -52,7 +52,7 @@ export default function ContactSection() {
               <img
                 src={github}
                 alt=''
-                className='md:w-[4vw] 5xl:w-[3vw] w-[11vw]'
+                className='landscape:md:w-[4vw] 5xl:w-[3vw] w-[11vw]'
               />
             </a>
             <img
@@ -62,7 +62,7 @@ export default function ContactSection() {
               aria-label='Gmail'
               src={gmail}
               alt=''
-              className=' md:w-[4vw] 5xl:w-[3vw] w-[10vw] hover:scale-125 ease-out duration-300 transition-all transform hover:rotate-12'
+              className=' landscape:md:w-[4vw] 5xl:w-[3vw] w-[10vw] hover:scale-125 ease-out duration-300 transition-all transform hover:rotate-12'
               onClick={handleEmailClick}
             />
           </div>
