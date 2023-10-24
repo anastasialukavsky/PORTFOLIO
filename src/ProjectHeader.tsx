9890395cd7e2d94f43b21806efa29ae68da72671
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-23 20:12:21 -0400

    add portr md styles

--- src/ProjectHeader.tsx
+++ src/ProjectHeader.tsx
@@ -6,7 +6,7 @@ export type ProjectHeaderProps = {
 export default function ProjectHeader({projectName}: ProjectHeaderProps) {
   return (
     <div>
-      <p className='text-center md:text-[1rem] lg:text-[1.2rem] xl:text-[1.4rem] 2xl:text-[2rem] text-[5vw] pt-5 md:pt-0 short:text-[1.6rem] text-[#121212] font-mono font-bold portrait:text-[1.5rem] portrait:pt-0'>
+      <p className='text-center md:text-[1rem] lg:text-[1.2rem] xl:text-[1.4rem] 2xl:text-[2rem] text-[5vw] pt-5 md:pt-0 short:text-[1.6rem] text-[#121212] font-mono font-bold portrait:text-[1.5rem] portrait:pt-0 portrait:md:pt-5 portrait:md:text-[1.8rem]'>
         {projectName}
       </p>
     </div>
