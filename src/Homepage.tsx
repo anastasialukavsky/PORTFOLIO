75cc8867d39bccd59dd0af9250e38622d164ceae
ruralFluffyDill <stacylukavsky@gmail.com>
2023-10-26 15:17:39 -0400

    change min w

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -40,7 +40,7 @@ export default function Homepage() {
   return (
     <main
       id='home'
-      className='container relative w-[100vw] h-full min-h-screen z-0 text-white'
+      className='container relative min-w-[100vw] h-full min-h-screen z-0 text-white'
     >
       <Navbar setNavbarHeight={setNavbarHeight} />
       <div className='blob-wrapper fixed self-center md:h-[100dvh] h-[100svh] w-[100svw]  z-[100] md:w-[100dvw] m-auto bottom-0 right-0  landscape:xshort:w-[100svw] landscape:xshort:h-[100svh]'>
